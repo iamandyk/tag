2089421b6e8bbc4716e164908075080f648f0482
Andy Kay <andykay.me@gmail.com>
2020-01-12 16:17:04 -0500

    Initial commit: setup Parcel, React, Typescript, SASS, and initial data fetch

---
+++ src/index.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { Component } from "react";
+import { render } from "react-dom";
+import "./index.scss";
+
+const App = () => {
+  const [users, setUsers] = React.useState([]);
+  React.useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch("https://randomuser.me/api/?results=10");
+      const json = await res.json();
+
+      setUsers(json.results);
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      {users.map((user, index) => (
+        <div key={index}>
+          <img
+            src={user.picture.large}
+            alt={`${user.name.first} ${user.name.last}'s Avatar`}
+          />
+          <div>
+            <div>{`${user.name.first} ${user.name.last}`}</div>
+            <div>{`${user.location.state}, ${user.location.country}`}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
